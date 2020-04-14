82e3c0f9474ce9cdba4ebc631a23c445b36dcda3
Evgeny Nazarovs <eugene.nazarovs@gmail.com>
2020-04-14 16:20:31 +0100

    passes first test

---
+++ specs/park_spec.js
@@ -0,0 +1,36 @@
+const assert = require('assert');
+const Park = require('../models/park.js');
+const Dinosaur = require('../models/dinosaur.js');
+
+describe('Park', function() {
+
+  let park;
+
+  beforeEach(function () {
+    park = new Park('Jurassic');
+  })
+
+  it('should have a name', function(){
+    const actual = park.name;
+    assert.strictEqual(actual, 'Jurassic')
+  });
+
+  xit('should have a ticket price');
+
+  xit('should have a collection of dinosaurs');
+
+  xit('should be able to add a dinosaur to its collection');
+
+  xit('should be able to remove a dinosaur from its collection');
+
+  xit('should be able to find the dinosaur that attracts the most visitors');
+
+  xit('should be able to find all dinosaurs of a particular species');
+
+  xit('should be able to calculate the total number of visitors per day');
+
+  xit('should be able to calculate the total number of visitors per year');
+
+  xit('should be able to calculate total revenue for one year');
+
+});
