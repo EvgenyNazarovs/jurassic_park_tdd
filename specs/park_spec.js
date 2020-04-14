32230167c4b69a5b22563180d6f400b43fd8412e
Evgeny Nazarovs <eugene.nazarovs@gmail.com>
2020-04-14 17:10:54 +0100

    can remove dinosaur from collection

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -6,10 +6,12 @@ describe('Park', function() {
 
   let park;
   let dinosaur1;
+  let dinosaur2;
 
   beforeEach(function () {
     park = new Park('Jurassic', 10);
     dinosaur1 = new Dinosaur('t-rex', 'carnivore', 50);
+    dinosaur2 = new Dinosaur('indominus rex', 'carnivore', 45);
   })
 
   it('should have a name', function(){
@@ -33,7 +35,13 @@ describe('Park', function() {
     assert.equal(actual, 1);
   });
 
-  xit('should be able to remove a dinosaur from its collection');
+  it('should be able to remove a dinosaur from its collection', function(){
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.removeDinosaur(dinosaur1);
+    const actual = park.dinosaurs[0];
+    assert.equal(actual, dinosaur2);
+  });
 
   xit('should be able to find the dinosaur that attracts the most visitors');
 
