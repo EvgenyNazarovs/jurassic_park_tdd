a0bf2574c7921fd6e864978952d637f3bd2dd8ae
Evgeny Nazarovs <eugene.nazarovs@gmail.com>
2020-04-14 16:41:11 +0100

    can add dinosaur to park's collection

--- models/park.js
+++ models/park.js
@@ -2,6 +2,11 @@ const Park = function(name, ticketPrice) {
   this.name = name;
   this.ticketPrice = ticketPrice;
   this.dinosaurs = [];
+
+}
+
+Park.prototype.addDinosaur = function(dinosaur) {
+  this.dinosaurs.push(dinosaur);
 }
 
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -5,9 +5,11 @@ const Dinosaur = require('../models/dinosaur.js');
 describe('Park', function() {
 
   let park;
+  let dinosaur1;
 
   beforeEach(function () {
     park = new Park('Jurassic', 10);
+    dinosaur1 = new Dinosaur('t-rex', 'carnivore', 50);
   })
 
   it('should have a name', function(){
@@ -25,7 +27,11 @@ describe('Park', function() {
     assert.deepStrictEqual(actual, [])
   });
 
-  xit('should be able to add a dinosaur to its collection');
+  it('should be able to add a dinosaur to its collection', function(){
+    park.addDinosaur(dinosaur1);
+    const actual = park.dinosaurs.length;
+    assert.equal(actual, 1);
+  });
 
   xit('should be able to remove a dinosaur from its collection');
 
